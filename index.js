40620ced48fc5e134dd58db84162dbe3cdc3e73c
ciphered <baptiste.crespy@gmail.com>
2020-09-13 16:31:27 +0200

    added a generic way to define error log files

--- index.js
+++ index.js
@@ -9,7 +9,14 @@ const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID
 
 // path to the files in which errors needs to be checked
 const ERROR_LOGS = [
-  '/var/log/nginx/error.log.1'
+  {
+    name: 'nginx',
+    path: '/var/log/nginx/error.log' 
+  },
+  {
+    name: 'nginx+1',
+    path: '/var/log/nginx/error.log.1'
+  }
 ];
 
 // setup bot with token ID
@@ -19,48 +26,50 @@ const main = async () => {
   // the main error object
   const errors = {};
 
-  // checks for possible errors in the logs
-  const error_logs = fs.readFileSync(ERROR_LOGS[0], {
-    encoding: 'utf-8',
-    flag: 'r',
-  })
-
-  // regex for YYYY/MM/DD
-  const date_regex = /\d{4}\/(0[1-9]|1[012])\/(0[1-9]|[12][0-9]|3[01])/g
-  // regex for HH:MM:SS
-  const time_regex = /(?:[01]\d|2[0123]):(?:[012345]\d):(?:[012345]\d)/g
-
-  const now = new Date().getTime();
-  const lines = error_logs.split('\n')
-
-  // will be changed if an error is detected
-  let error = null;
-
-  for (const line of lines) {
-    // we check for a data within the line
-    const date = line.match(date_regex)
-    const time = line.match(time_regex)
-
-    if (date && time) { // a resut was found
-      let timestamp = new Date(`${date[0]} ${time[0]}`).getTime();
-      let hours_diff = (now - timestamp) / 1000 / 3600;
-
-      // if an error was spotted in the last 4 hours, we want to add it to the report
-      if (hours_diff <= 100) {
-        error = line;
-        break;
-      }
-
-      // if the difference is greater than what's required, we just discard everything, it means no error was found
-      if (hours_diff > 100) {
-        break;
+  for (let error_log of ERROR_LOGS) {
+    // checks for possible errors in the logs
+    const error_logs = fs.readFileSync(error_log.path, {
+      encoding: 'utf-8',
+      flag: 'r',
+    })
+  
+    // regex for YYYY/MM/DD
+    const date_regex = /\d{4}\/(0[1-9]|1[012])\/(0[1-9]|[12][0-9]|3[01])/g
+    // regex for HH:MM:SS
+    const time_regex = /(?:[01]\d|2[0123]):(?:[012345]\d):(?:[012345]\d)/g
+  
+    const now = new Date().getTime();
+    const lines = error_logs.split('\n')
+  
+    // will be changed if an error is detected
+    let error = null;
+  
+    for (const line of lines) {
+      // we check for a data within the line
+      const date = line.match(date_regex)
+      const time = line.match(time_regex)
+  
+      if (date && time) { // a resut was found
+        let timestamp = new Date(`${date[0]} ${time[0]}`).getTime();
+        let hours_diff = (now - timestamp) / 1000 / 3600;
+  
+        // if an error was spotted in the last 4 hours, we want to add it to the report
+        if (hours_diff <= 1) {
+          error = line;
+          break;
+        }
+  
+        // if the difference is greater than what's required, we just discard everything, it means no error was found
+        if (hours_diff > 1) {
+          break;
+        }
       }
     }
-  }
-
-  // do we have an error ?
-  if (error) {
-    errors['nginx'] = error;
+  
+    // do we have an error ?
+    if (error) {
+      errors[error_log.name] = error;
+    }
   }
 
 
